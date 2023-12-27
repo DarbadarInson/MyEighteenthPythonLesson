e06faa16ad9fd8f2d910e2d12321846bed56a1d7
Ozodbek <devanabuildmuber@gmail.com>
2023-12-28 04:53:17 +0500

    this is my Python Lesson 18

---
+++ data.py
@@ -0,0 +1,12 @@
+contacts = {
+    "James" : {
+        "phone_number" : 123456789,
+        "email" : "johhny@gmail.com"
+    },
+    "Jenny":{
+        "phone_number": 987654321,
+        "email": "jenny@gmail.com"
+    }
+}
+
+print(contacts["James"]["phone_number"])
---
+++ myPython18.py
@@ -0,0 +1,9 @@
+from data import contacts
+
+JennyEmail = contacts["Jenny"]["email"]
+print(JennyEmail)
+
+def myFunction(n1, n2):
+    total = n1 + n2
+    return total
+print(myFunction(3,4))
